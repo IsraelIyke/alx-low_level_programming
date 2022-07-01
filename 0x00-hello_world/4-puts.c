d3bc981c0bb5be707262ef1747ea2eb4f85c6506
IsraelIyke <nwangwuisrael@gmail.com>
2022-07-01 11:13:35 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - Entry point
-* Return: 0 if Successful
-*/
+ * main - Entry point
+ * Return: 0 Always
+ */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle")
 	return (0);
 }
