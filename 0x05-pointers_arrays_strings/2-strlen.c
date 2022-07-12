1bf190206b76b0d2c868a7a032227462618c0f68
IsraelIyke <nwangwuisrael@gmail.com>
2022-07-12 07:50:15 +0100

    intro

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * Pointer
- * 
- * @s: pointer to int
- * Return: 0 always
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
 
 int _strlen(char *s)
