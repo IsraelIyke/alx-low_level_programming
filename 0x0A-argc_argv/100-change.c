8787d513454932fca45930c3c3f8a7356c9913eb
IsraelIyke <nwangwuisrael@gmail.com>
2022-07-22 06:20:07 -0700

    arg

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,44 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+/**
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
+int isInteger(const char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+	if (s[i] < '0' || s[i] > '9')
+		return (0);
+	i++;
+}
+return (1);
+}
 
 /**
- * main - prints the minimum number of coins to
- * make change for an amount of money
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 (Success), 1 (Error)
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char const *argv[])
 {
-int num, j, result;
+int i = 0, coinUsed = 0, coin = 0;
 int coins[] = {25, 10, 5, 2, 1};
 
 if (argc != 2)
 {
-printf("Error\n");
-return (1);
+	printf("Error\n");
+	return (1);
+}
+if (isInteger(argv[1]))
+{
+	i = atoi(argv[1]);
+	while (i > 0 && coin <= 4)
+	{
+		if (i >= coins[coin])
+		{
+			i -= coins[coin];
+			coinUsed++;
+		}
+		else
+		{
+			coin++;
+		}
+	}
 }
 
-    num = atoi(argv[1]);
-    result = 0;
+printf("%i\n", coinUsed);
 
-    if (num < 0)
-    {
-        printf("0\n");
-        return (0);
-    }
-
-    for (j = 0; j < 5 && num >= 0; j++)
-    {
-        while (num >= coins[j])
-        {
-            result++;
-            num -= coins[j];
-        }
-    }
-
-    printf("%d\n", result);
-    return (0);
+return (0);
 }
+
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,85 +1,45 @@
 #include <stdio.h>
-#include "main.h"
-
+#include <stdlib.h>
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * isInteger - checks if s is an integer
+ * @s: string to check
+ * Return: 0 or 1
  */
-int _atoi(char *s)
+
+int isInteger(const char *s)
 {
-int i, d, n, len, f, digit;
-
-i = 0;
-d = 0;
-n = 0;
-len = 0;
-f = 0;
-digit = 0;
-
-while (s[len] != '\0')
-len++;
-
-while (i < len && f == 0)
+int i = 0;
+while (s[i] != '\0')
 {
-if (s[i] == '-')
-++d;
-
-if (s[i] >= '0' && s[i] <= '9')
-{
-digit = s[i] - '0';
-if (d % 2)
-digit = -digit;
-n = n * 10 + digit;
-f = 1;
-if (s[i + 1] < '0' || s[i + 1] > '9')
-break;
-f = 0;
+	if (s[i] < '0' || s[i] > '9')
+		return (1);
+	i++;
 }
-i++;
-}
-
-    if (f == 0)
-        return (0);
-
-    return (n);
+return (0);
 }
 
 /**
- * main - adds two positive number
- * @argc: number of arguments
- * @argv: array of arguents
- *
- * Return: 0 (Success), or 1 (Success)
+ * main - adds positive numbers
+ * @argc: int
+ * @argv: list
+ * Return: 0
  */
-int main(int argc, char *argv[])
+
+int main(int argc, char const *argv[])
 {
-    int sum, num, i, j, k;
-
-    sum = 0;
-
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] > '9' || argv[i][j] < '0')
-            {
-                puts("Error");
-                return (1);
-            }
-        }
-    }
-
-    for (k = 1; k < argc; k++)
-    {
-        num = _atoi(argv[k]);
-        if (num >= 0)
-        {
-            sum += num;
-        }
-    }
-
-    printf("%d\n", sum);
-    return (0);
+int sum = 0;
+while (--argc)
+{
+	if (isInteger(argv[argc]))
+	{
+		printf("Error\n");
+		return (1);
+	}
+	sum += atoi(argv[argc]);
 }
+
+printf("%i\n", sum);
+
+return (0);
+}
+
