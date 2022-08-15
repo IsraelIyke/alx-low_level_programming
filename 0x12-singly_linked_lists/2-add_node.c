14b9e397c067b0c5076fb36f6e3c845530641ad5
IsraelIyke <nwangwuisrael@gmail.com>
2022-08-15 16:25:39 -0700

    linked

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * add_node - adds nodes
+ * @head: argument
+ * @str: argument
+ * Return: new node added
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *ptr = head;
+int i;
+
+if (head == NULL)
+{
+	ptr->str = str;
+	ptr->next = NULL;
+}
+else
+{
+	for (i = 0; ptr->next == NULL)
+	{
+		ptr->str = str;
+		ptr = ptr->next;
+	}
+ptr->next = NULL;
+}
+return (head);
+}
+
